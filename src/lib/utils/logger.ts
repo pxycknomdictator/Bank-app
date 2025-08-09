e1181ebb09ba836c67c962eb3c30f92699607107
Noman <pxycknomdictator@outlook.com>
2025-08-09 12:46:34 +0500

    add winston logger for logging and debugging in codebase

---
+++ src/lib/utils/logger.ts
@@ -0,0 +1,39 @@
+import winston from "winston";
+import { NODE_ENV } from "$env/static/private";
+
+const { combine, timestamp, colorize, printf, uncolorize } = winston.format;
+
+const formatter = printf(({ level, message, timestamp, ...meta }) => {
+	let log = `${timestamp} [${level}]: ${message}`;
+	if (Object.keys(meta).length > 0) log += ` ${JSON.stringify(meta)}`;
+
+	return log;
+});
+
+export const logger = winston.createLogger({
+	level: NODE_ENV === "production" ? "info" : "debug",
+	format: combine(timestamp(), uncolorize(), formatter),
+	transports: [
+		new winston.transports.File({
+			filename: "logs/error.log",
+			level: "error",
+			format: combine(timestamp(), uncolorize(), formatter)
+		}),
+		new winston.transports.File({
+			filename: "logs/combined.log",
+			format: combine(timestamp(), uncolorize(), formatter)
+		})
+	],
+	exceptionHandlers: [
+		new winston.transports.File({
+			filename: "logs/exceptions.log",
+			format: combine(timestamp(), uncolorize(), formatter)
+		})
+	]
+});
+
+if (NODE_ENV !== "production") {
+	logger.add(
+		new winston.transports.Console({ format: combine(colorize(), timestamp(), formatter) })
+	);
+}
