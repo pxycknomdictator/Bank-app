f600313a3de6c59a0a1c730abf32e2dc4be5d1e9
Noman <pxycknomdictator@outlook.com>
2025-08-09 16:19:44 +0500

    add cloudinary upload and destroy utility functions

--- src/lib/utils/cloud.ts
+++ src/lib/utils/cloud.ts
@@ -1,4 +1,6 @@
+import fs from "node:fs/promises";
 import { v2 as cloudinary } from "cloudinary";
+import { logger } from "$lib/utils/logger";
 import * as env from "$env/static/private";
 
 cloudinary.config({
@@ -7,3 +9,53 @@ cloudinary.config({
 	api_secret: env.CLOUDINARY_API_SECRET,
 	secure: true
 });
+
+interface CloudResponse {
+	publicId: string;
+	secureUrl: string;
+	fileType: string;
+}
+
+export async function uploadFileOnCloud(
+	fileLocation: string,
+	folder: string = "bank-app"
+): Promise<CloudResponse> {
+	try {
+		const response = await cloudinary.uploader.upload(fileLocation, {
+			folder,
+			resource_type: "auto"
+		});
+
+		await fs.unlink(fileLocation);
+
+		return {
+			publicId: response.public_id,
+			secureUrl: response.secure_url,
+			fileType: response.resource_type
+		};
+	} catch (error) {
+		logger.error("cloud.ts -> Error uploading file to Cloudinary:", { error });
+		await fs.unlink(fileLocation);
+		throw error;
+	}
+}
+
+export async function deleteFileFromCloud(
+	publicId: string,
+	resource_type: string = "auto"
+): Promise<boolean> {
+	try {
+		const response = await cloudinary.uploader.destroy(publicId, {
+			resource_type
+		});
+
+		if (response.result !== "ok") {
+			throw new Error(`cloud.ts -> Cloudinary deletion failed: ${response.result}`);
+		}
+
+		return true;
+	} catch (error) {
+		logger.error("cloud.ts -> Error deleting file from Cloudinary:", { error });
+		throw error;
+	}
+}
